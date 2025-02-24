84c3189ace4a6ae80d995749576b4dc94bff1fef
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2025-02-23 19:02:31 -0800

    Updated README and requirements.txt

--- indexer.py
+++ indexer.py
@@ -1,4 +1,11 @@
 # No Lucene PyLucene or ElasticSearch
 
+import re
+import json
+import os
+import math
+import lxml
+from bs4 import BeautifulSoup
+from collections import defaultdict
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
